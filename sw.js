8c62a7dd1b006b97f0dda99b8bdfa3025b0c08ec
ara0202cjy <ara0202@qq.com>
2026-09-10 15:01:24 +0800

    sync: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dgr-xiaozhangben-v1";
+const CACHE = "dgr-xiaozhangben-v2";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
