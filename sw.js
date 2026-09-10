18b7bdc99f0195d6759fd9c4be280f2c0bc0b1dd
ara0202cjy <ara0202@qq.com>
2026-09-10 17:45:47 +0800

    deploy sw.js (v9, ledger tabs + income visible)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dgr-xiaozhangben-v8";
+const CACHE = "dgr-xiaozhangben-v9";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("message", e => { if (e.data === "skip") self.skipWaiting(); });
