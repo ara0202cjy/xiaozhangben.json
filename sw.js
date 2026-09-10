b1b721f16b5923b3fa62670db08a9784689d36a5
ara0202cjy <ara0202@qq.com>
2026-09-10 17:24:08 +0800

    deploy sw.js (settings refactor)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dgr-xiaozhangben-v6";
+const CACHE = "dgr-xiaozhangben-v7";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("message", e => { if (e.data === "skip") self.skipWaiting(); });
