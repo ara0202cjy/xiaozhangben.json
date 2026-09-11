0b8b743a06ad8287b95e8fe87f8c46db8f01e92a
ara0202cjy <ara0202@qq.com>
2026-09-11 09:18:34 +0800

    deploy 隐私模式 v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dgr-xiaozhangben-v12";
+const CACHE = "dgr-xiaozhangben-v13";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg", "./apple-touch-icon.png"];
 
 self.addEventListener("message", e => { if (e.data === "skip") self.skipWaiting(); });
